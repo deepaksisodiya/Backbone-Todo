9fbb67b618231c2050db1d9ee209aefcda4aeca9
deepak sisodiya <deepak.sisodiya@gmail.com>
2014-12-10 02:19:22 +0530

    model code

---
+++ src/model/todo.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Deepak Sisodiya on 10/12/14.
+ */
+
+
+var Todo = Backbone.Model.extend({
+
+	defaults : {
+		title : "",
+		completed : false
+	},
+
+	initialize : function() {
+		console.log("todo model");
+	},
+
+	setCompleted : function() {
+		this.set({"completed" : true});
+	},
+
+	setIncompleted : function() {
+		this.set({"completed" : false});
+	},
+
+	isCompleted : function() {
+		return this.get("completed");
+	}
+
+});
+
+var todo = new Todo();
